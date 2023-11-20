09ca4879721f4288d5722b31c3b5d74f7f90e048
EOsgood1 <92339384+EOsgood1@users.noreply.github.com>
2023-11-20 12:10:40 -0800

    mV calculation fix

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-// main program for a PID DC motor controller implementation on 
+// main program for a PID DC motor controller implementation on
 // the tm4c123gh6m eval board.
 
 #include "TM4C123GH6PM.h"
 #include "tm4c123gh6pm_def.h"
 #include <stdio.h>
-
 
 // define the number of ticks per thread time (2ms thread switching time)
 #define TIMESLICE 32000
@@ -27,50 +26,52 @@ void OS_Suspend(void);
 void OS_Signal(int32_t *s);
 void OS_Wait(int32_t *s);
 
-
 extern void Init_LCD_Ports(void);
-extern void Display_Msg(char* Str);
+extern void Display_Msg(char *Str);
 extern void Set_Position(uint32_t POS);
 extern void Init_LCD(void);
 extern void Init_Keypad();
 
-extern void Hex2ASCII(char* output, int hex);
+extern void Hex2ASCII(char *output, int hex);
 extern uint8_t Read_Key();
 uint8_t Key_ASCII;
 
 int32_t Current_speed(int32_t Avg_volt);
 
-
 void delayMs(int n)
 {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 6265; j++) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 6265; j++)
+		{
 			// do nothing for 1 ms
 		}
 	}
 }
 
 /*
-    Application specific functions
+	Application specific functions
 */
 
 void init_adc_pins(void)
 {
 	// the ADC uses ports A,B,C,E
 	SYSCTL->RCGCGPIO |= 0x17;
-	while ((SYSCTL->PRGPIO & 0x17) == 0) {};
-	GPIOC->DIR |= 0x80; 	// set PC7 for output
+	while ((SYSCTL->PRGPIO & 0x17) == 0)
+	{
+	};
+	GPIOC->DIR |= 0x80; // set PC7 for output
 	GPIOC->DEN |= 0x80;
 	GPIOC->DATA |= 0x80;
-	
-	GPIOE->DIR &= 0x0E;	// set PE1-3 to be input
-	GPIOE->DEN |= 0x0E;		// DEN PE1-3
 
-	GPIOB->DIR &= ~0x3C;	// PB2-5 as input
+	GPIOE->DIR &= 0x0E; // set PE1-3 to be input
+	GPIOE->DEN |= 0x0E; // DEN PE1-3
+
+	GPIOB->DIR &= ~0x3C; // PB2-5 as input
 	GPIOB->DEN |= 0x3C;
-	
+
 	GPIOA->DIR &= ~0xC0;
-	GPIOA->DEN |= 0xC0;		// PA6-7 as input
+	GPIOA->DEN |= 0xC0; // PA6-7 as input
 }
 
 // configure PWM module 1 to generate a PWM signal on
@@ -79,25 +80,25 @@ void Init_M1PWM6(int period_ms, int duty_cycle)
 {
 	// given a wave period in ms, compute the load value
 	int load_val = (250000 / period_ms) - 1;
-  
-	SYSCTL->RCGCPWM |= 0x02; // enable clock to PWM1
-	SYSCTL->RCGCGPIO |= 0x20; // enable clock to GPIOF
-	SYSCTL->RCGCGPIO |= 0x02; // enable clock to GPIOB
-	delayMs(1); // PWM1 seems to take a while to start
-	SYSCTL->RCC |= 0x001E0000; // use divider 64
-	PWM1->_3_CTL = 0; // disable PWM1_3 during configuration
+
+	SYSCTL->RCGCPWM |= 0x02;	// enable clock to PWM1
+	SYSCTL->RCGCGPIO |= 0x20;	// enable clock to GPIOF
+	SYSCTL->RCGCGPIO |= 0x02;	// enable clock to GPIOB
+	delayMs(1);					// PWM1 seems to take a while to start
+	SYSCTL->RCC |= 0x001E0000;	// use divider 64
+	PWM1->_3_CTL = 0;			// disable PWM1_3 during configuration
 	PWM1->_3_GENA = 0x00000C08; // low on load, high on CPMA down
-	PWM1->_3_LOAD = load_val; 
+	PWM1->_3_LOAD = load_val;
 	PWM1->_3_CMPB = duty_cycle;
-	PWM1->_3_CTL = 1; // enable PWM1_3
-	PWM1->ENABLE |= 0x40; // enable PWM1
-	GPIOF->DIR |= 0x04; // set PF3 pins as output (LED) pin
-	GPIOF->DEN |= 0x04; // set PF3 pins as digital pins
-	GPIOF->AFSEL |= 0x04; // enable alternate function
+	PWM1->_3_CTL = 1;			// enable PWM1_3
+	PWM1->ENABLE |= 0x40;		// enable PWM1
+	GPIOF->DIR |= 0x04;			// set PF3 pins as output (LED) pin
+	GPIOF->DEN |= 0x04;			// set PF3 pins as digital pins
+	GPIOF->AFSEL |= 0x04;		// enable alternate function
 	GPIOF->PCTL &= ~0x00000F00; // clear PF2 alternate function
-	GPIOF->PCTL |= 0x00000500; // set PF2 alternate function to PWM
-	GPIOB->DEN |= 0x03; // PB0-1 as digital pins
-	GPIOB->DIR |= 0x03; // set PB0-1 as output
+	GPIOF->PCTL |= 0x00000500;	// set PF2 alternate function to PWM
+	GPIOB->DEN |= 0x03;			// PB0-1 as digital pins
+	GPIOB->DIR |= 0x03;			// set PB0-1 as output
 }
 
 void MOT34_Dir_Set(void)
@@ -111,20 +112,20 @@ void MOT34_Speed_Set(uint16_t duty)
 	PWM1->_3_CMPB = duty - 1;
 }
 
-// Init Timer0A to generate interrupts every 100 us. 
+// Init Timer0A to generate interrupts every 100 us.
 // The handler will sample the voltage output by the
 // ADC.
 void Init_Timer0A(uint32_t period_us)
 {
-  SYSCTL->RCGCTIMER |= 0x01; 	// activate timer0
-	TIMER0->CTL &= ~0x01;				// disable timer during setup
+	SYSCTL->RCGCTIMER |= 0x01;		// activate timer0
+	TIMER0->CTL &= ~0x01;			// disable timer during setup
 	TIMER0->CFG = 0x00000000;		// put timer in 16 bit mode
-	TIMER0->TAMR = 0x00000002;				// config timer for periodic mode
-	TIMER0->TAILR = period_us * 16;				// set the count limit
-	TIMER0->IMR |= 0x01;				// timer interrupts on time-out event
+	TIMER0->TAMR = 0x00000002;		// config timer for periodic mode
+	TIMER0->TAILR = period_us * 16; // set the count limit
+	TIMER0->IMR |= 0x01;			// timer interrupts on time-out event
 	NVIC_EN0_R |= 1 << 19;
-	NVIC_PRI4_R |= 0x40000000;	// set priority level 2
-	TIMER0->ICR = 0x01;					// clear the status bit
+	NVIC_PRI4_R |= 0x40000000; // set priority level 2
+	TIMER0->ICR = 0x01;		   // clear the status bit
 	TIMER0->CTL |= 0x01;
 	// NOTE: here, right before this function returns, we would normally enable
 	// interrupts globally, but that is done in StartOS in osasm_V2.s (so we don't)
@@ -135,7 +136,7 @@ int timer_count = 0;
 // init to unlikely val
 int8_t ADC_OUTPUT = 0xFF;
 int ADC_sum = 0;
-int current_speed =0x0A0A; //replace this value with current speed
+int current_speed = 0x0A0A; // replace this value with current speed
 
 // Read the value output by the ADC
 void TIMER0A_Handler(void)
@@ -143,22 +144,23 @@ void TIMER0A_Handler(void)
 	// NOTE: we should either make this interrupt the highest priority in the system,
 	//			 or disable all other interrupts while servicing it to prevent a critical code
 	// 			 section from being interrupted.
-	TIMER0->ICR = 0x01;		// ack the interrupt.
-	
+	TIMER0->ICR = 0x01; // ack the interrupt.
+
 	GPIOC->DATA &= ~0x80;
 	GPIOC->DATA |= 0x80;
-	
-	while((GPIOE->DATA & 0x02) == 0)
+
+	while ((GPIOE->DATA & 0x02) == 0)
 	{
 	}
-	
+
 	ADC_OUTPUT = (GPIOA->DATA & 0xC0) | (GPIOB->DATA & 0x3C) | ((GPIOE->DATA & 0x0C) >> 2);
 	ADC_sum += ADC_OUTPUT;
 	timer_count++;
-	
-	if(timer_count >= 100){
+
+	if (timer_count >= 100)
+	{
 		int ADC_avg = ADC_sum / timer_count;
-		float v_avg = (((float)ADC_avg / 128.0) * 10.0) / 1000.0;
+		int v_avg = ADC_avg * 10000 / 128;
 		current_speed = Current_speed(v_avg);
 		timer_count = 1;
 	}
@@ -166,24 +168,24 @@ void TIMER0A_Handler(void)
 
 void lcd_thread(void)
 {
-  while (1) {
+	while (1)
+	{
 		/*Routine to print all necessary values to LCD*/
 		Set_Position(0x00);
 		Display_Msg("Input RPM:");
 
-		int input_RPM =0x0A; //replace this value with average speed
+		int input_RPM = 0x0A; // replace this value with average speed
 		char RPM_str[4];
-		char* RPM_ptr = RPM_str;
+		char *RPM_ptr = RPM_str;
 		Hex2ASCII(RPM_ptr, input_RPM);
 		Display_Msg(RPM_ptr);
-
 
 		Set_Position(0x40);
 		Display_Msg("T:");
 
-		int average_speed =0x270F; //replace this value with average speed
+		int average_speed = 0x270F; // replace this value with average speed
 		char avg_str[4];
-		char* avg_ptr = avg_str;
+		char *avg_ptr = avg_str;
 		Hex2ASCII(avg_ptr, average_speed);
 		Display_Msg(avg_ptr);
 
@@ -191,7 +193,7 @@ void lcd_thread(void)
 		Display_Msg("C:");
 
 		char cur_str[4];
-		char* cur_ptr = cur_str;
+		char *cur_ptr = cur_str;
 		Hex2ASCII(cur_ptr, current_speed);
 		Display_Msg(cur_ptr);
 	};
@@ -200,32 +202,32 @@ void lcd_thread(void)
 // LED thread responsible for dequeuing and outputting to led.
 void thread2(void)
 {
-    while (1) {
-			t2++;
-		};
-
+	while (1)
+	{
+		t2++;
+	};
 }
 
 void motor_init()
 {
-	Init_M1PWM6(100, 0);	 	// init the motor speed to zero.
-	MOT34_Dir_Set();				// set the direction using PB0-1
-	MOT34_Speed_Set(2500 / 2); 	// set the motor speed to 50%
+	Init_M1PWM6(100, 0);	   // init the motor speed to zero.
+	MOT34_Dir_Set();		   // set the direction using PB0-1
+	MOT34_Speed_Set(2500 / 2); // set the motor speed to 50%
 }
 
 int main(void)
 {
-		init_adc_pins();
-		Init_Timer0A(100); 			// initalize for 100us interrupts
-		motor_init();
-		
-		Init_LCD_Ports();
-    Init_LCD();
+	init_adc_pins();
+	Init_Timer0A(100); // initalize for 100us interrupts
+	motor_init();
 
-    OS_Init();                 // initialize, disable interrupts, 16 MHz
+	Init_LCD_Ports();
+	Init_LCD();
 
-    OS_AddThreads(&lcd_thread, &thread2);
+	OS_Init(); // initialize, disable interrupts, 16 MHz
 
-    OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
-    return 0;             // this never executes
+	OS_AddThreads(&lcd_thread, &thread2);
+
+	OS_Launch(TIMESLICE); // doesn't return, interrupts enabled in here
+	return 0;			  // this never executes
 }
